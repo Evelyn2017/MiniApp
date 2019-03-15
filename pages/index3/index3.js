7ff08f563b2e19a7aa261553bbd56fa33809122f
Evelyn2017 <evelinx@foxmail.com>
2019-03-15 15:47:32 +0800

    data binding

---
+++ pages/index3/index3.js
@@ -0,0 +1,74 @@
+// pages/index3/index3.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    msg: "hello world! 1",
+    num: 1000,
+    isBoy: true,
+    person:{
+      "name":"Evelyn",
+      "height":160
+    },
+    title:"This is a new page index3"
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index5/index5.js
@@ -0,0 +1,71 @@
+// pages/index5/index5.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    arr:["apple","banana",'water melon'],
+    person:{
+      name:"Evelyn",
+      height:160
+    }
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
